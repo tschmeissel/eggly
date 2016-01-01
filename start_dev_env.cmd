42218b4eb30a08be6963dedd5adc0c313b2fb539
Zaphod Beblerox <zaphod.beblerox@github.com>
2016-01-02 00:49:15 +0100

    loading tomcat added

--- start_dev_env.cmd
+++ start_dev_env.cmd
@@ -1,3 +1,6 @@
+cd ..\..\
+call bin\startup.bat
+cd webapps\eggly
 call start "" "%APPDATA%\..\Local\Programs\Git\bin\sh.exe" --login
 call "C:\Program Files (x86)\Notepad++\notepad++.exe" -openSession "notepad++.session"
 
