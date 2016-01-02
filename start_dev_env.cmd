05ca8878b6f44dff34398a1d7fe57b68e05ae036
Zaphod Beblerox <zaphod.beblerox@github.com>
2016-01-02 01:22:36 +0100

    loading chrome incognito introduced but stops loading notepad afterwards

--- start_dev_env.cmd
+++ start_dev_env.cmd
@@ -1,6 +1,14 @@
 cd ..\..\
 call bin\startup.bat
 cd webapps\eggly
+
+
 call start "" "%APPDATA%\..\Local\Programs\Git\bin\sh.exe" --login
+
+REM TODO make sure loading does not stop after loading chrome
+REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -incognito
+
 call "C:\Program Files (x86)\Notepad++\notepad++.exe" -openSession "notepad++.session"
+
+
 
