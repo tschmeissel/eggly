32a0ca2115d76de835c992d48055d7b7360aa35b
Zaphod Beblerox <zaphod.beblerox@github.com>
2016-01-02 01:23:09 +0100

    refactor to data model

---
+++ app/common/models/categories-models.js
@@ -0,0 +1,18 @@
+(function() {
+	angular.module('eggly.models.categories', [])
+		.service('CategoriesModel', function() {
+			var model = this;
+			
+			categories = [
+				{"id": 0, name: "Development"},
+				{"id": 1, name: "Design"},
+				{"id": 2, name: "Exercise"},
+				{"id": 3, name: "Humor"}
+			];
+			
+			model.getCategories = function() {
+				return categories;
+			}
+		});
+
+})();
